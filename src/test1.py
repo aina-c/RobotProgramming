caf38881815510bd678aa4b1d37cfc5c20d48c95
aina-c <aina.crespihromcova@gmail.com>
2025-09-25 10:21:29 +0200

    src folder added

---
+++ src/test1.py
@@ -0,0 +1,14 @@
+git clone <repository_url> #download the project
+
+#hacer paara subir cambios
+git add . #upload, add changes to the staging area
+git commit -m "description" #commit changes
+git push #push changes to the remote repository 
+
+
+git config --global user.name "aina-c"
+git config --global user.email "aina.crespihromcova@gmail.com"
+
+git status
+
+
